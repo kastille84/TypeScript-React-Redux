f4546ad8ee6d47f3942879cd7466bb40e09508bb
Edwin <Edwin.Martinez@people10.com>
2020-02-20 13:32:49 -0500

    added comment for alternative setup

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -3,6 +3,8 @@ import {FetchTodosAction, DeleteTodoAction} from './todos';
 //this is opinionated,
 //check documentation of typescript & redux
 //for alternative on how to set up ActionTypes
+//or watch this video for alternative way of setting up
+//https://www.youtube.com/watch?v=dZZxegovK9Q  @5:50 mark
 export enum ActionTypes {
   fetchTodos,
   deleteTodo
